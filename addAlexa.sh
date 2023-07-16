a33497f693c16f39a25d00f42d71b87754a3f7f9
Mark Liebrand <developer@liebrand.eu>
2023-07-16 16:05:06 +0200

    Update addAlexa.sh

--- addAlexa.sh
+++ addAlexa.sh
@@ -6,7 +6,16 @@ cd WiFi-remote-for-Bestway-Lay-Z-SPA
 git checkout f9bc945089aa7423dfab254ec7958aef355861f1
 cd ..
 cd ./fauxmoESP
-t checkout 0b3aee19eac2797ac125aaab50b78a2a52e6ecf6
+git checkout 0b3aee19eac2797ac125aaab50b78a2a52e6ecf6
 cd ..
+echo "ADDING fauxmoESP to WIFI-remote"
 mkdir -p ./WiFi-remote-for-Bestway-Lay-Z-SPA/Code/lib/fauxmoESP
 cp ./fauxmoESP/src/* ./WiFi-remote-for-Bestway-Lay-Z-SPA/Code/lib/fauxmoESP
+echo "Patching fauxmoESP.cpp"
+patch ./WiFi-remote-for-Bestway-Lay-Z-SPA/Code/lib/fauxmoESP/fauxmoESP.cpp ./fauxmoESP.cpp.patch
+echo "Patching fauxmoESP.h"
+patch ./WiFi-remote-for-Bestway-Lay-Z-SPA/Code/lib/fauxmoESP/fauxmoESP.h ./fauxmoESP.h.patch
+echo "Patching main.cpp"
+patch ./WiFi-remote-for-Bestway-Lay-Z-SPA/Code/src/main.cpp ./main.cpp.patch
+echo "Patching main.h"
+patch ./WiFi-remote-for-Bestway-Lay-Z-SPA/Code/src/main.h ./main.h.patch
